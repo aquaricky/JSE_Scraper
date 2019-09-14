85f40ccceeb4dd7711933e33c5f7c04f7e423b05
DESKTOP-HDS02QJ\Jade <jade.a.miller93@gmail.com>
2019-09-14 16:49:46 -0500

    implemented filereader
    added the base code for the file reader utility to feed json from the config file to the web scraper

--- index.js
+++ index.js
@@ -12,6 +12,7 @@
 
 // Add the required system Packages
 const request_module = require('./scraper-module/request-module.js');
+const fr = require('./support_modules/filereader.js')
 
 //Load config file or in environment variables using process.env.PORT
 //var config  = require(''); //Set this as the config js file
@@ -19,5 +20,14 @@ const request_module = require('./scraper-module/request-module.js');
 //Configuring custom logger
 //==============================================================================
 
-request_module.sendurlrequest();
-request_module.sendurlrequestDynamic();
+//request_module.sendurlrequest();
+//request_module.sendurlrequestDynamic();
+
+fr.jsonReader('./config/urlconfig.json', (err, config) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log(config) // => "Infinity Loop Drive"
+})
+//console.log();
